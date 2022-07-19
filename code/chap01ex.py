a2d5d4ca49aa9e735f4eabc78ff64cf1eceb8884
simpleParadox <abomicode@gmail.com>
2022-07-19 16:34:01 -0600

    Completed chapter 1

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -14,6 +14,42 @@ import nsfg
 import thinkstats2
 
 
+def ReadFemResp(dct_file='2002FemResp.dct',
+                dat_file='2002FemResp.dat.gz',
+                nrows=None):
+    """Reads the NSFG respondent data.
+
+    dct_file: string file name
+    dat_file: string file name
+
+    returns: DataFrame
+    """
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip', nrows=nrows)
+    CleanFemResp(df)
+    return df
+
+def CleanFemResp(df):
+    """Recodes variables from the respondent frame.
+
+    df: DataFrame
+    """
+    pass
+
+
+def ValidateResults(resp):
+    
+    preg = nsfg.ReadFemPreg()
+    preg_map = nsfg.MakePregMap(preg)
+    for index, pregnum in resp.pregnum.items():
+        caseid = resp.caseid[index]
+        indices = preg_map[caseid]
+        
+        if len(indices) != pregnum:
+            return False
+    return True
+        
+
 def main(script):
     """Tests the functions in this module.
 
@@ -23,4 +59,9 @@ def main(script):
 
 
 if __name__ == '__main__':
-    main(*sys.argv)
+    resp = ReadFemResp()
+    print(resp.pregnum.value_counts().sort_index())
+    print(ValidateResults(resp))
+
+    
+    
